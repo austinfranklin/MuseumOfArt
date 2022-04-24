bd0c4983c0ce466228646c56f962e42808e21bfe
Austinfranklin12 <55801294+Austinfranklin12@users.noreply.github.com>
2022-04-24 16:56:01 -0500

    Made more changes

--- script.js
+++ script.js
@@ -9,9 +9,16 @@ window.addEventListener("load", function() {
         fetch(action, {
             method: 'POST',
             body: data,
-        })
+        });
     });
-    createText();
+});
+
+// clears text box after submitting something
+let button = document.getElementById('submit');
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    let input = document.getElementById('input');
+    input.value = '';
 });
 
 // code for retreiving and displaying responses
@@ -26,23 +33,25 @@ let parse;
 let text;
 // used to store all responses and prevent duplicates
 let allResponses = [];
+let animate = [];
 
-Papa.parse(
-    "https://docs.google.com/spreadsheets/d/1dMEF1bC9AuGRLN34wEfA6pBxsRev8cBJyUI-_5ZiDf4/pub?output=csv",
+setInterval(Papa.parse(
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRbATeczAPZBiWkuoTX7p-2IIg012MwAyHQx1Dno8fcqV3nZct8tYIf5I_VgNjILvfToO6rXyYlkADN/pub?output=csv",
     {
         download: true,
         header: true,
+        fastMode: true,
+        //skipEmptyLines: true,
         //worker: true,
         complete: (results) => {
             console.log(results);
             responses = results;
-            createText();
         }
     }
-);
+), 50); // refreshes every 500 ms
 
-function createText(parse = responses) {
-    //let row = 0, col = 0;
+setInterval(function createText(parse = responses) {
+    
     console.log("Data: ", parse.data);
 
     for (i in parse.data) {
@@ -63,12 +72,17 @@ function createText(parse = responses) {
 
             let display = document.getElementById("responses");
             display.appendChild(textDiv);
+        } else if (responses == false) {
+            allResponses = [];
         } else {
             allResponses.splice(i, 1);
         }
     };
-};
+}, 500); // Adds new text every 500 ms
 
 function checkIfDuplicateExists(arr) {
     return new Set(arr).size !== arr.length;
 }
+
+let textPos;
+let speed;
